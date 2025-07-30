6e050833accffa2b6e11383f1b2ce14e8dbc821e
Lucky-tech10 <ezihegoodluck59@gmail.com>
2025-07-30 09:57:21 +0100

    Implement the error handlers' structure and folder. data folder and auth controller structure

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
+require("dotenv").config();
+require("express-async-errors");
+
 const express = require("express");
+
+// routes
+const authRoutes = require("./src/routes/authRoutes");
+
+// error handler
+const notFoundMiddleware = require("./src/middleware/not-found");
+const errorHandlerMiddleware = require("./src/middleware/error-handler");
 
 const app = express();
 
@@ -7,6 +17,12 @@ app.use(express.json());
 app.get("/api/v1", (req, res) => {
   res.send("Welcome to the auto mart API!");
 });
+
+// routes
+app.use("/api/v1/auth", authRoutes);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const PORT = process.env.PORT || 5000;
 
